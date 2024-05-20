05e619058394620d4026ecc16ba228629c1decfe
Kgomotso Nacane <kgomotsonacane@gmail.com>
2024-05-20 09:39:06 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,31 @@ const navMenu = document.querySelector('.navInfoMobile, .cancelIcon');
 const nav = document.querySelector('.menuIcon, .logo');
 const body = document.querySelector('body');
 const mobileLinks = document.querySelectorAll('.mobileLinks');
+const menu = document.querySelector('.hamItems');
+const mobileNav = document.querySelector('.mobNav');
+const ham = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.closeBtn');
+const hide = document.querySelector('.footerSocialIconsContainer');
 
-// menu mobile function
+menu.classList.add('invisible');
+
+ham.addEventListener('click', () => {
+  menu.classList.remove('invisible');
+  mobileNav.classList.add('invisible');
+  hide.classList.add('invisible');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileNav.classList.remove('invisible');
+  menu.classList.add('invisible');
+  hide.classList.remove('invisible');
+});
+
+menu.addEventListener('click', () => {
+  menu.classList.add('invisible');
+  mobileNav.classList.remove('invisible');
+  hide.classList.remove('invisible');
+});
 button.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   nav.classList.toggle('dNone');
@@ -77,3 +100,4 @@ window.addEventListener('load', () => {
     areaMsg.value = data.areaMsg;
   }
 });
+
